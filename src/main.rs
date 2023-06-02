c78d2883536345f954b3ef3a86a30904304aac7d
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2023-06-02 16:30:12 +1000

    refactor: change feature name to `csr` in `cfg` attires

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[cfg(not(any(feature = "ssr", feature = "ssg")))]
+#[cfg(not(any(feature = "ssr", feature = "csr")))]
 pub fn main() {
     // no client-side main function
     // unless we want this to work with e.g., Trunk for pure client-side testing
@@ -39,7 +39,7 @@ pub fn main() {
     // see optional feature `ssg` instead
 }
 
-#[cfg(all(not(feature = "ssr"), feature = "ssg"))]
+#[cfg(all(not(feature = "ssr"), feature = "csr"))]
 pub fn main() {
     // a client-side main function is required for using `trunk serve`
     // prefer using `cargo leptos serve` instead
