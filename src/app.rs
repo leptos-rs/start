f6666f9b83600b82a15fbf9cfdb7e9fd55b27b56
Greg Johnston <greg.johnston@gmail.com>
2023-06-30 08:15:09 -0400

    format

--- src/app.rs
+++ src/app.rs
@@ -7,9 +7,7 @@ pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
-    view! {
-        cx,
-
+    view! { cx,
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
