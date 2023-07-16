359531190f2a5d4c317962dc09c4236555eaf3cc
greg <greg@greg.lol>
2023-07-16 19:33:11 -0400

    SSG to CSR

--- src/main.rs
+++ src/main.rs
@@ -54,14 +54,14 @@ pub fn main() {
     // no client-side main function
     // unless we want this to work with e.g., Trunk for pure client-side testing
     // see lib.rs for hydration function instead
-    // see optional feature `ssg` instead
+    // see optional feature `csr` instead
 }
 
 #[cfg(all(not(feature = "ssr"), feature = "csr"))]
 pub fn main() {
     // a client-side main function is required for using `trunk serve`
     // prefer using `cargo leptos serve` instead
-    // to run: `trunk serve --open --features ssg`
+    // to run: `trunk serve --open --features csr`
     use leptos::*;
     use leptos_start::app::*;
     use wasm_bindgen::prelude::wasm_bindgen;
