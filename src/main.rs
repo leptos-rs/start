0012041c0189bd3cb1c9372b6a73cbdc7b62051f
Greg Johnston <greg.johnston@gmail.com>
2023-10-23 06:20:34 -0400

    fix CSR

--- src/main.rs
+++ src/main.rs
@@ -65,9 +65,5 @@ pub fn main() {
 
     console_error_panic_hook::set_once();
 
-    leptos::mount_to_body(move |cx| {
-        // note: for testing it may be preferrable to replace this with a
-        // more specific component, although leptos_router should still work
-        view! {cx, <App/> }
-    });
+    leptos::mount_to_body(App);
 }
